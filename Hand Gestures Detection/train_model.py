e91d86e8623a1103212369890124713556d20958
Irash Perera <irash.21@cse.mrt.ac.lk>
2024-06-08 10:34:00 +0530

    covert into numpy array

--- Hand Gestures Detection/train_model.py
+++ Hand Gestures Detection/train_model.py
@@ -1,5 +1,3 @@
-import cv2
-import mediapipe as mp
 import numpy as np
 import pickle
 
@@ -9,8 +7,8 @@ from sklearn.metrics import accuracy_score
 
 data_dict = pickle.load(open('./Hand Gestures Detection/data.pkl', 'rb'))
 
-data = data_dict['data']
-labels = data_dict['labels']
+data = np.asarray(data_dict['data'])
+labels = np.asarray(data_dict['labels'])
 
 X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, shuffle=True, stratify=labels)
 
